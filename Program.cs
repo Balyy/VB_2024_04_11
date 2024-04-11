adb2d6908b1ceecde49249728a186a897930bf74
viragh.balazs <viragh.balazs@T15-10>
2024-04-11 11:32:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VB_2024_04_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+        }
+    }
+}
